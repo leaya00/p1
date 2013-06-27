112655f18840cb5b539670faa84a63c87edd0b0f
lxy <xxx@xxx.com>
2013-06-27 09:12:29 +0800

    test

---
+++ manage/user.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>	
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>	
+	<meta http-equiv="X-UA-Compatible" content="IE=7" />
+	<script src="../js/jquery-1.7.1.min.js" type="text/javascript"></script>
+    
+</head>
+<body>
+	
+   
+</body>
+</html>
